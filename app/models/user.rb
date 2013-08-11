f5f080bea89ad911b172dc6ca190fc87a18516a8
Marlon <marlonmisra@gmail.com>
2013-08-11 17:20:20 -0400

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
@@ -12,6 +13,10 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
